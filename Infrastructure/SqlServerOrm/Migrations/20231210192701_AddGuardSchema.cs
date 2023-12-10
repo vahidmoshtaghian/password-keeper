0ffde29d5c1e88d971e2e3db864ef627615f8da0
Vahid Moshtaghian <vahid.moshtagh@gmail.com>
2023-12-10 22:57:28 +0330

    make migration

---
+++ Infrastructure/SqlServerOrm/Migrations/20231210192701_AddGuardSchema.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SqlServerOrm.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGuardSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Guard");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                schema: "Actor",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                schema: "Guard",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Passwords",
+                schema: "Guard",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passwords", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Memberships",
+                schema: "Guard",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsOwner = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    OrganizationId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memberships", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Memberships_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalSchema: "Guard",
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Memberships_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Actor",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Credentials",
+                schema: "Guard",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrganizationId = table.Column<long>(type: "bigint", nullable: false),
+                    PasswordId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Credentials", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Credentials_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalSchema: "Guard",
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Credentials_Passwords_PasswordId",
+                        column: x => x.PasswordId,
+                        principalSchema: "Guard",
+                        principalTable: "Passwords",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAccesses",
+                schema: "Guard",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsCreator = table.Column<bool>(type: "bit", nullable: false),
+                    MembershipId = table.Column<long>(type: "bigint", nullable: false),
+                    PasswordId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccesses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAccesses_Memberships_MembershipId",
+                        column: x => x.MembershipId,
+                        principalSchema: "Guard",
+                        principalTable: "Memberships",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserAccesses_Passwords_PasswordId",
+                        column: x => x.PasswordId,
+                        principalSchema: "Guard",
+                        principalTable: "Passwords",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Credentials_OrganizationId",
+                schema: "Guard",
+                table: "Credentials",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Credentials_PasswordId",
+                schema: "Guard",
+                table: "Credentials",
+                column: "PasswordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_OrganizationId",
+                schema: "Guard",
+                table: "Memberships",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_UserId",
+                schema: "Guard",
+                table: "Memberships",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAccesses_MembershipId",
+                schema: "Guard",
+                table: "UserAccesses",
+                column: "MembershipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAccesses_PasswordId",
+                schema: "Guard",
+                table: "UserAccesses",
+                column: "PasswordId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Credentials",
+                schema: "Guard");
+
+            migrationBuilder.DropTable(
+                name: "UserAccesses",
+                schema: "Guard");
+
+            migrationBuilder.DropTable(
+                name: "Memberships",
+                schema: "Guard");
+
+            migrationBuilder.DropTable(
+                name: "Passwords",
+                schema: "Guard");
+
+            migrationBuilder.DropTable(
+                name: "Organizations",
+                schema: "Guard");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                schema: "Actor",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
