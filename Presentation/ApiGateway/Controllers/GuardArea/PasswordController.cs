6a63657b55b41af16f419c1a50766d72b103fa71
Vahid Moshtaghian <vahid.moshtagh@gmail.com>
2024-01-06 20:04:30 +0330

    make controller

---
+++ Presentation/ApiGateway/Controllers/GuardArea/PasswordController.cs
@@ -0,0 +1,41 @@
+﻿using ApiGateway.Controllers.Base;
+using ApiGateway.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.Controllers.GuardArea;
+
+[ApiExplorerSettings(GroupName = SwaggerDefinition.GuardArea)]
+public class PasswordController : ApplicationController
+{
+    [HttpGet("{id:long}")]
+    public async Task GetById([FromRoute] long id)
+    {
+
+    }
+
+    [HttpDelete("{id:long}")]
+    public async Task Delete([FromRoute] long id)
+    {
+
+    }
+
+    [HttpPatch("{id:long}")]
+    public async Task Update([FromRoute] long id, [FromBody] object command)
+    {
+
+    }
+
+    [HttpGet("organization/{organizationId:long}")]
+    public async Task<List<object>> Get([FromRoute] long organizationId)
+    {
+
+    }
+
+    [HttpPost("organization/{organizationId:long}")]
+    public async Task Add([FromRoute] long organizationId, [FromBody] object command)
+    {
+
+    }
+
+
+}
